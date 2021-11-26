5c1feaadec4a6a7441bf00695f374f04d7ff5917
Dmitriy <paranin-dmitriy@mail.ru>
2021-11-26 17:25:16 +0300

    add homework 2

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import s from "./Message.module.css";
 
-
-
-
-type PropsType = {
-    avatar:any
-    name:string
-    message:string
-    time:string
+export type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
 }
 
 export function Message(props: PropsType) {
     return (
         <div className={s.message}>
-            <img className={s.img} src={props.avatar} alt=""/>
+            <img className={s.img} src={props.avatar} alt="avatar"/>
+            <div className={s.angle}/>
 
             <div className={s.textMessage}>
                 <div className={s.name}>{props.name}</div>
-                <div>{props.message}</div>
+                <div className={s.text}>{props.message}</div>
                 <div className={s.time}>{props.time}</div>
             </div>
 
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import s from './Affairs.module.css'
 
-type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
+type AffairsPropsType = { // need to fix any+++
+    data: Array<AffairType>
+    setFilter: (filter:FilterType) => void
     deleteAffairCallback: any
 }
 
@@ -17,20 +18,20 @@ function Affairs(props: AffairsPropsType) {
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => {props.setFilter('all')} // need to fix+++
+    const setHigh = () => {props.setFilter('high')}
+    const setMiddle = () => {props.setFilter('middle')}
+    const setLow = () => {props.setFilter('low')}
 
     return (
-        <div>
+        <div className={s.showAllAffairs}>
 
             {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
+            <button className={s.button} onClick={setAll}>All</button>
+            <button className={s.button} onClick={setHigh}>High</button>
+            <button className={s.button} onClick={setMiddle}>Middle</button>
+            <button className={s.button} onClick={setLow}>Low</button>
         </div>
     )
 }
